d76052d30e83b2e5179f06f2186f01f188be0a09
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2023-04-06 20:25:08 -0600

    created angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
